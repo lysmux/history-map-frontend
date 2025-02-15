244d360bafa65ccaa6378cf638bfa978bbd23f20
lysmux <kllraz@yandex.ru>
2025-02-15 20:15:23 +0300

    fix: leaflet icons not display after build

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './plugins/leaflet' // fix leaflet icons after build
+
 const app = createApp(App)
 
 app.use(router)
