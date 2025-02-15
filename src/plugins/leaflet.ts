07a064c307d47fa304ceca5bcb6cddfa0e39caaf
lysmux <kllraz@yandex.ru>
2025-02-15 20:15:23 +0300

    fix: leaflet icons not display after build

---
+++ src/plugins/leaflet.ts
@@ -0,0 +1,14 @@
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+import L from 'leaflet'
+
+// @ts-expect-error https://github.com/Leaflet/Leaflet/issues/4968
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
