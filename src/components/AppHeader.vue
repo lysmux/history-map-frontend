898bb3b3de403dcf90168582eee2e652ee6f7c3c
lysmux <kllraz@yandex.ru>
2025-02-20 13:05:59 +0300

    fix: header overlapped content

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute top-0 left-0 w-full z-100000 opacity-75 flex justify-between items-center shadow-xl px-10 py-3 bg-slate-200"
+    class="z-1000 flex justify-between items-center shadow-xl px-10 py-3 bg-slate-200"
   >
     <a href="https://itmo.ru"
       ><img src="@/assets/itmo-logo-dark.svg" alt="ITMO logo" class="w-30"
